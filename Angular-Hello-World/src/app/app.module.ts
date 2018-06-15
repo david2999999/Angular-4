8fb6ac47e0ed3d244a6500eb87500b1adec51bee
David Jiang <djiang86@binghamton.edu>
2018-06-15 15:21:11 +0000

    simple angular fundamental exercise

--- Angular-Hello-World/src/app/app.module.ts
+++ Angular-Hello-World/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppComponent } from './app.component';
 import {CourseComponent} from './course.component';
 import { CoursesComponent } from './courses/courses.component';
 import {CoursesService} from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import {AuthorsService} from './authors.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    CoursesComponent
+    CoursesComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [CoursesService],
+  providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
