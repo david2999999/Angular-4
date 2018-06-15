e4c4456f98e058083bfa1f40de77f6922f63fd6b
David Jiang <djiang86@binghamton.edu>
2018-06-15 12:19:28 +0000

    constructor

--- Angular-TypeScript/src/app/typescript/objects.ts
+++ Angular-TypeScript/src/app/typescript/objects.ts
@@ -1,18 +1,27 @@
-class Point {
+class Point2 {
   x: number;
   y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  // making the constructor parameters optional
+  // constructor(x?: number, y?: number) {
+  //   this.x = x;
+  //   this.y = y;
+  // }
 
   draw() {
     // ...
     console.log(`X: ${this.x}, Y: ${this.y}`);
   }
 
-  getDistance(another: Point) {
+  getDistance(another: Point2) {
     // ...
   }
 }
 
-let point = new Point();
-point.x = 1;
-point.y = 2;
+let point = new Point2(1, 2);
 point.draw();
