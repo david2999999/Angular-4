6f769777d1535cc2bb284baf84d9e832178d81c9
David Jiang <djiang86@binghamton.edu>
2018-06-16 19:25:54 +0000

    alias with input property

--- Angular-Reusable-Components/src/app/component-api/component-api.component.ts
+++ Angular-Reusable-Components/src/app/component-api/component-api.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./component-api.component.css']
 })
 export class ComponentApiComponent implements OnInit {
-  @Input() isFavorite: boolean;
+  @Input('is-favorite') isFavorite: boolean;
 
   constructor() { }
 
