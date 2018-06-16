73c02a2c685599a7b70a87abef5daadd388eaf4a
David Jiang <djiang86@binghamton.edu>
2018-06-16 20:44:45 +0000

    ng-container to just render the textContent

--- Angular-Reusable-Components/src/app/app.module.ts
+++ Angular-Reusable-Components/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ComponentApiComponent } from './component-api/component-api.component';
 import { NgContentComponent } from './ng-content/ng-content.component';
+import { NgContainerComponent } from './ng-container/ng-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentApiComponent,
-    NgContentComponent
+    NgContentComponent,
+    NgContainerComponent
   ],
   imports: [
     BrowserModule,
