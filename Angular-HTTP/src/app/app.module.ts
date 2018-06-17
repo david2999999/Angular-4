26ead8737a0564cc8a8c7e45ddf813432d67c51f
David Jiang <djiang86@binghamton.edu>
2018-06-17 23:30:38 +0000

    sending a get request to retrieve data

--- Angular-HTTP/src/app/app.module.ts
+++ Angular-HTTP/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
