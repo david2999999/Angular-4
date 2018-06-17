e66c0af820ca1a28138fca39b0adfeb157163291
David Jiang <djiang86@binghamton.edu>
2018-06-17 20:59:14 +0000

    simple async validation with promise

--- Angular-Reactive-Form/src/app/signup-form/username.validators.ts
+++ Angular-Reactive-Form/src/app/signup-form/username.validators.ts
@@ -1,4 +1,5 @@
 import {AbstractControl, ValidationErrors} from '@angular/forms';
+import {P} from '@angular/core/src/render3';
 
 export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
@@ -6,5 +7,17 @@ export class UsernameValidators {
       return { cannotContainSpace: true };
     }
     return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'tim') {
+          resolve({ shouldBeUnique: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
   }
 }
