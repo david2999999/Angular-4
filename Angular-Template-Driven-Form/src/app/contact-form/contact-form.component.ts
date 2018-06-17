f8a6aa9e99aab46cc9ef3b982c4e185ebf4f80f7
David Jiang <djiang86@binghamton.edu>
2018-06-17 16:51:12 +0000

    working with dropdown list

--- Angular-Template-Driven-Form/src/app/contact-form/contact-form.component.ts
+++ Angular-Template-Driven-Form/src/app/contact-form/contact-form.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Phone'},
+    {id: 3, name: 'Mail'}
+  ]
   constructor() { }
 
   ngOnInit() {
