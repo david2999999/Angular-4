e093e02a23913e4e54faa684ed63abf2bdd9ded7
David Jiang <djiang86@binghamton.edu>
2018-06-19 14:55:22 +0000

    ngOnDestory to unsubscribe to observables

--- Angular-Firebase/src/app/app.component.ts
+++ Angular-Firebase/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   courses: any[];
+  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-     db.list('/course').valueChanges()
+     this.subscription = db.list('/course').valueChanges()
           .subscribe(courses => {
             this.courses = courses;
             console.log(this.courses);
           });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
