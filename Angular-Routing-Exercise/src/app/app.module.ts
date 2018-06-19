079828a2f11ca57150439227b59e1c92363e0527
David Jiang <djiang86@binghamton.edu>
2018-06-19 00:27:16 +0000

    completed routing exercise

--- Angular-Routing-Exercise/src/app/app.module.ts
+++ Angular-Routing-Exercise/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ArchiveComponent } from './archive/archive.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArchiveComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'archive/:year/:month', component: ArchiveComponent},
+      { path: '**', component: NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
