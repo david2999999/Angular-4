8e635790afc84679e72ccf7734deb506c60adc80
David Jiang <djiang86@binghamton.edu>
2018-06-19 02:21:57 +0000

    displaying/hiding elements depending on the login status of the user

--- Angular-Authentication/src/app/services/auth.service.ts
+++ Angular-Authentication/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import {JwtHelper, tokenNotExpired} from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -25,7 +26,19 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return false;
+    return tokenNotExpired();
+    
+    // const jwtHelper = new JwtHelper();
+    // const token = localStorage.getItem('token');
+    //
+    // if (!token) {
+    //   return false;
+    // }
+    //
+    // const expirationDate = jwtHelper.getTokenExpirationDate(token);
+    // const isExpired = jwtHelper.isTokenExpired(token);
+    //
+    // return !isExpired;
   }
 }
 
