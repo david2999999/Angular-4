4f7a39c85ba903358b6e8f441542bfe535e913cf
David Jiang <djiang86@binghamton.edu>
2018-06-19 15:01:35 +0000

    using async pipe to automatically sub and unsub to observers

--- Angular-Firebase/src/app/app.component.ts
+++ Angular-Firebase/src/app/app.component.ts
@@ -1,25 +1,27 @@
 import {Component, OnDestroy} from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
-  courses: any[];
-  subscription: Subscription;
+export class AppComponent {
+  courses$;
+  // courses: any[];
+  // subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-     this.subscription = db.list('/course').valueChanges()
-          .subscribe(courses => {
-            this.courses = courses;
-            console.log(this.courses);
-          });
+    this.courses$ =  db.list('/course').valueChanges();
+     // this.subscription = db.list('/course').valueChanges()
+     //      .subscribe(courses => {
+     //        this.courses = courses;
+     //        console.log(this.courses);
+     //      });
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
 }
