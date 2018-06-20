af0accf1ae39377cc4281405d7cadfabc3e2f90e
David Jiang <djiang86@binghamton.edu>
2018-06-20 13:54:04 +0000

    select dropdown list

--- Angular-Material/src/app/app.module.ts
+++ Angular-Material/src/app/app.module.ts
@@ -4,22 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material';
+import {MatCheckboxModule, MatSelectModule} from '@angular/material';
 import { MatRadioModule } from '@angular/material/radio';
 import { RadioButtonComponent } from './radio-button/radio-button.component';
 import { CheckBoxComponent } from './check-box/check-box.component';
+import { SelectComponent } from './select/select.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     RadioButtonComponent,
-    CheckBoxComponent
+    CheckBoxComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatCheckboxModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSelectModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
