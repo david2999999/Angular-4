077b8111875c2e9914922e46fdd167e8c3168412
David Jiang <djiang86@binghamton.edu>
2018-06-20 01:53:17 +0000

    using keyframes

--- Angular-Animation/src/app/animations.ts
+++ Angular-Animation/src/app/animations.ts
@@ -1,4 +1,4 @@
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 export let fade = trigger('fade', [
   state('void', style({ opacity: 0, padding: 0, height: 0, fontSize: 0 })),
@@ -14,6 +14,9 @@ export let slide = trigger('slide', [
     animate(500)
   ]),
   transition(':leave', [
-    animate('.4s cubic-bezier(.17,.67,1,-0.08)', style({ transform: 'translateX(-100%)'}))
+    animate('.6s ease-out', keyframes([
+      style({ offset: .2, opacity: 1, transform: 'translateX(20px)'}),
+      style({ offset: 1, opacity: 0, transform: 'translateX(-100%)'}),
+    ]))
   ])
 ]);
