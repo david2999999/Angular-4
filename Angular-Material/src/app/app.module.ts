a536bd6846205062dffb4f1433addcf4cd4afce1
David Jiang <djiang86@binghamton.edu>
2018-06-20 14:54:47 +0000

    input fields

--- Angular-Material/src/app/app.module.ts
+++ Angular-Material/src/app/app.module.ts
@@ -4,19 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCheckboxModule, MatSelectModule} from '@angular/material';
+import {MatCheckboxModule, MatInputModule, MatSelectModule} from '@angular/material';
 import { MatRadioModule } from '@angular/material/radio';
 import { RadioButtonComponent } from './radio-button/radio-button.component';
 import { CheckBoxComponent } from './check-box/check-box.component';
 import { SelectComponent } from './select/select.component';
 import {FormsModule} from '@angular/forms';
+import { InputsComponent } from './inputs/inputs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RadioButtonComponent,
     CheckBoxComponent,
-    SelectComponent
+    SelectComponent,
+    InputsComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +26,8 @@ import {FormsModule} from '@angular/forms';
     BrowserAnimationsModule,
     MatCheckboxModule,
     MatRadioModule,
-    MatSelectModule
+    MatSelectModule,
+    MatInputModule
 
   ],
   providers: [],
