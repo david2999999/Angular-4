aa79703b1b790426a9b5f5bb127fc3a3a414c780
David Jiang <djiang86@binghamton.edu>
2018-06-20 00:32:52 +0000

    fade in animation

--- Angular-Animation/src/app/todos/todos.component.ts
+++ Angular-Animation/src/app/todos/todos.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('fade', [
+      // state(),
+      transition('void => *', [
+        style({
+          backgroundColor: '#ecf0f1', opacity: 0, padding: 0, height: 0
+        }),
+        animate(200)
+      ])
+    ])
+  ]
 })
 export class TodosComponent {
   items: any[] = [
-    'Wash the dishes', 
-    'Call the accountant', 
+    'Wash the dishes',
+    'Call the accountant',
     'Apply for a car insurance'];
 
   addItem(input: HTMLInputElement) {
     this.items.splice(0, 0, input.value);
-    input.value = ''; 
+    input.value = '';
   }
 
   removeItem(item) {
