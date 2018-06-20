9fa8fb7da7b52ed80dbc56ddd78ba8fe08b1982e
David Jiang <djiang86@binghamton.edu>
2018-06-20 13:14:59 +0000

    installed angular material, added checkbox

--- Angular-Material/src/app/app.module.ts
+++ Angular-Material/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
