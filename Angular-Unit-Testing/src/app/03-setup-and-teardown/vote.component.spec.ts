61ba495dc83eb9433e1fe3df1824c917fb1abd7f
David Jiang <djiang86@binghamton.edu>
2018-06-21 15:13:28 +0000

    testing components, using beforeEachg

--- Angular-Unit-Testing/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ Angular-Unit-Testing/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,29 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
-describe('VoteComponent', () => {
-  it('', () => {
+describe('VoteComponent', () => {    // Arrange
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  // afterEach(() => {
+  // 
+  // })
+
+  it('should increment totalVotes when upvoted', () => {
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+    // Act
+    component.downVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(-1);
   });
 });
