ef27ba1bebf55d66272c43201d1c31c31ede6e34
David Jiang <djiang86@binghamton.edu>
2018-06-23 19:09:37 +0000

    authguard for admin page

---
+++ Angular-Project/src/app/admin-auth-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+import {UserService} from './user.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private userService: UserService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.user$
+                .switchMap(user => this.userService.get(user.uid))
+                .map(appUser => appUser['isAdmin']);
+  }
+}
