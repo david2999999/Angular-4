e00f1ade662fbc75f97191469ae9c8f9fafd4b31
David Jiang <djiang86@binghamton.edu>
2018-06-23 16:04:59 +0000

    extracted login and logout to a service

--- Angular-Project/src/app/app.module.ts
+++ Angular-Project/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
 import {RouterModule} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AuthService} from './auth.service';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,9 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
       { path: 'admin/orders', component: AdminOrdersComponent},
     ])
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
