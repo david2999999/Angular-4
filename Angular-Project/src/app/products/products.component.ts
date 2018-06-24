48f5c91abd0b92b0089497f19f3b80c5c1146b88
David Jiang <djiang86@binghamton.edu>
2018-06-24 18:53:10 +0000

    filtering products based on category

--- Angular-Project/src/app/products/products.component.ts
+++ Angular-Project/src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../services/product.service';
 import {CategoryService} from '../services/category.service';
+import {ActivatedRoute} from '@angular/router';
+import {Product} from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -8,13 +10,28 @@ import {CategoryService} from '../services/category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
   constructor(private productService: ProductService,
-              private categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+              private categoryService: CategoryService,
+              private route: ActivatedRoute) {
+
+    productService.getAll()
+      .subscribe(products => {
+        this.products = products;
+      });
+
     this.categories$ = this.categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) :
+        this.products;
+    });
   }
 
   ngOnInit() {
