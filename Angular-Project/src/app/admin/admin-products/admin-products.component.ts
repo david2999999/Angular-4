4c201dcb1ce6705b53f07bbb4b91181cc05f17c7
David Jiang <djiang86@binghamton.edu>
2018-06-24 15:26:24 +0000

    searching the list of products

--- Angular-Project/src/app/admin/admin-products/admin-products.component.ts
+++ Angular-Project/src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from '../../services/product.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  product$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.product$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
 
   ngOnInit() {
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p['title'].toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+
 
 }
