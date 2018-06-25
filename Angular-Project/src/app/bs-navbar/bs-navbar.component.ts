1c3a812c395dba508eefbb547cec8ffc8191989e
David Jiang <djiang86@binghamton.edu>
2018-06-25 23:01:29 +0000

    shopping cart item interface

--- Angular-Project/src/app/bs-navbar/bs-navbar.component.ts
+++ Angular-Project/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 import {AppUser} from '../models/app-user';
+import {ShoppingCartService} from '../shopping-cart.service';
 
 @Component({
   selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit{
   appUser: AppUser;
 
-  constructor(private authService: AuthService) {
-    authService.appUser$.subscribe(appUser => this.appUser = appUser);
+  constructor(private authService: AuthService, private shoppingCartService: ShoppingCartService) {}
+
+  async ngOnInit() {
+    this.authService.appUser$.subscribe(appUser => this.appUser = appUser);
+    const cart$ =  await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      cart.items
+    });
   }
 
   logout() {
