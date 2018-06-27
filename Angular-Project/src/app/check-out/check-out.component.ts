36ce634c5245d466cbabdffeba7a6b2ea01415b5
David Jiang <djiang86@binghamton.edu>
2018-06-27 02:53:24 +0000

    extract order model

--- Angular-Project/src/app/check-out/check-out.component.ts
+++ Angular-Project/src/app/check-out/check-out.component.ts
@@ -4,6 +4,7 @@ import {ShoppingCart} from '../models/shopping-cart';
 import {Subscription} from 'rxjs';
 import {OrderService} from '../order.service';
 import {AuthService} from '../services/auth.service';
+import {Order} from '../models/order';
 
 @Component({
   selector: 'app-check-out',
@@ -32,23 +33,7 @@ export class CheckOutComponent implements OnInit, OnDestroy{
   }
 
   placeOrder() {
-    const order = {
-      userId: this.userId,
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map(item => {
-        return {
-          product: {
-            title: item.title,
-            imageUrl: item.imageUrl,
-            price: item.price
-          },
-          quantity: item.quantity,
-          totalPrice: item.totalPrice
-        };
-      })
-    };
-
+    const order = new Order(this.userId, this.shipping, this.cart);
     this.orderService.storeOrder(order);
   }
 
