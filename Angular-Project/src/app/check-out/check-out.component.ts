7c32cbb5efc122730227a059bb430ad9ca504e0b
David Jiang <djiang86@binghamton.edu>
2018-06-27 02:07:58 +0000

    created order service

--- Angular-Project/src/app/check-out/check-out.component.ts
+++ Angular-Project/src/app/check-out/check-out.component.ts
@@ -1,14 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ShoppingCartService} from '../shopping-cart.service';
+import {ShoppingCart} from '../models/shopping-cart';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent {
+export class CheckOutComponent implements OnInit, OnDestroy{
   shipping = {};
+  cart: ShoppingCart;
+  subscription: Subscription;
+
+  constructor(private shoppingCartService: ShoppingCartService) {}
+
+  async ngOnInit() {
+    const cart$ = await this.shoppingCartService.getCart();
+    this.subscription = cart$.subscribe(cart => this.cart = cart);
+  }
 
   placeOrder() {
-    console.log(this.shipping);
+    const order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(item => {
+        return {
+          product: {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            price: item.price
+          },
+          quantity: item.quantity,
+          totalPrice: item.totalPrice
+        };
+      })
+    };
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
