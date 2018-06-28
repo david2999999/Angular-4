540552c3bd04c4d5c982c3e18554d2c27140bbf7
David Jiang <djiang86@binghamton.edu>
2018-06-28 00:40:40 +0000

    extract shopping form componenet

---
+++ Angular-Project/src/app/shipping-form/shipping-form.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Order} from '../models/order';
+import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
+import {OrderService} from '../order.service';
+import {AuthService} from '../services/auth.service';
+import {ShoppingCart} from '../models/shopping-cart';
+
+@Component({
+  selector: 'app-shipping-form',
+  templateUrl: './shipping-form.component.html',
+  styleUrls: ['./shipping-form.component.css']
+})
+export class ShippingFormComponent implements OnInit, OnDestroy {
+  @Input('cart') cart: ShoppingCart;
+  shipping = {};
+  userId: string;
+  userSubscription: Subscription;
+
+  constructor(private orderService: OrderService,
+              private authService: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.userSubscription = this.authService.user$.subscribe(user => {
+      this.userId = user.uid;
+    });
+  }
+
+  async placeOrder() {
+    const order = new Order(this.userId, this.shipping, this.cart);
+    const result = await this.orderService.placeOrder(order);
+    this.router.navigate(['/order-success', result.key]);
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+}
